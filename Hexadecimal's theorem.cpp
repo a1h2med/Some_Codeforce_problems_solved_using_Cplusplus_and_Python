ae04ad2be279aa0edf713f00c97d877c6453fde4
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2020-07-14 11:34:50 +0200

    Add files via upload

---
+++ Hexadecimal's theorem.cpp
@@ -0,0 +1,12 @@
+// solution to 199/A on codeforces
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+    int number = 0;
+    cin >> number;
+    cout <<0 << " " << 0 << " " << number;
+    return 0;
+}
